9537859470674f8f1602a1526c047d512a5747da
luis miguel gonzalez <luismg8812@hotmail.com>
2019-04-04 13:40:05 -0500

    ajustes en keys de la base de datos

--- build/keys.js
+++ build/keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'ec2-23-23-173-30.compute-1.amazonaws.com',
-        user: 'qrxrjxfaddxbfg',
-        password: '551e57eeb8fd144a4d23f220cadca97cc5a7ee0ee8970d760920e43184dd0750',
-        database: 'dd08vuuok947e2',
+        host: 'ec2-54-225-95-183.compute-1.amazonaws.com',
+        user: 'sraillogfqjvfh',
+        password: 'f4467b4ad687c77b212d8bbacd5583fcd937325cbee34d044ec7adfd50ee5d48',
+        database: 'd4mn73eg995brt',
         port: 5432,
         ssl: true
     }
