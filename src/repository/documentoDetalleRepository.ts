cc1d715e4d98685b73756aa845520eebe9320b47
luis miguel gonzalez <luismg8812@hotmail.com>
2020-01-23 09:48:46 -0500

    se agrega eliminar a ot

--- src/repository/documentoDetalleRepository.ts
+++ src/repository/documentoDetalleRepository.ts
@@ -2,7 +2,7 @@
 
 class DocumentoDetalleRepository{
     public getIdDocumentoDetalle:string="select nextval('s_documento_detalle')"; 
-    public getDocumentoDetalleByDocumento:string="select * from DOCUMENTO_DETALLE where documento_id = $1"; 
+    public getDocumentoDetalleByDocumento:string="select * from DOCUMENTO_DETALLE where estado=1 and documento_id = $1"; 
     
 }
 
