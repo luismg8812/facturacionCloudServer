8aad9b7760910af304f53655ca297d3fcaed838f
luis miguel gonzalez <luismg8812@hotmail.com>
2020-02-29 19:28:34 -0500

    ajuste

--- src/index.ts
+++ src/index.ts
@@ -23,26 +23,9 @@ class Server{
     config():void{
         this.app.set("port",process.env.PORT ||9090);
         this.app.use(morgan('dev'));
-        this.app.use(cors());
+        this.app.use(cors({origin: 'https://client2019.herokuapp.com'}));
+        //this.app.use(cors({origin: 'http://localhost:4200'}));
         this.app.use(express.json());
-        this.app.use(function (req, res, next) {
-
-            // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', '*');
-        
-            // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        
-            // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        
-            // Set to true if you need the website to include cookies in the requests sent
-            // to the API (e.g. in case you use sessions)
-            res.setHeader('Access-Control-Allow-Credentials', 'true');
-        
-            // Pass to next layer of middleware
-            next();
-        });
     }
 
     router():void{
