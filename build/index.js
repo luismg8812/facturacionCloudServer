4d4e5f2e0833b4636d54648e5971689d4c3d50c4
luis miguel gonzalez <luismg8812@hotmail.com>
2020-02-29 19:20:25 -0500

    ajuste en los cors

--- build/index.js
+++ build/index.js
@@ -27,6 +27,19 @@ class Server {
         this.app.use(morgan_1.default('dev'));
         this.app.use(cors_1.default());
         this.app.use(express_1.default.json());
+        this.app.use(function (req, res, next) {
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
+            // Pass to next layer of middleware
+            next();
+        });
     }
     router() {
         this.app.use(indexRoutes_1.default);
