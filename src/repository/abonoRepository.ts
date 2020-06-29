6fcb6ce40e110215b53855dc70b2808c737f2873
luis miguel gonzalez <luismg8812@hotmail.com>
2020-06-29 17:33:46 -0500

    subir

--- src/repository/abonoRepository.ts
+++ src/repository/abonoRepository.ts
@@ -1,6 +1,6 @@
 
 
-class AbonoRepository{
+class AbonoRepository{ 
   
     public getIdAbono:string="select nextval('s_abono')";
     public getAbonosByDocumento:string="select * from abono where documento_id = $1";
