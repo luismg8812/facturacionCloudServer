b041b80e3c3a8e02a3c8d34d88afdc0aa2dc7f2f
luis miguel gonzalez <luismg8812@hotmail.com>
2020-07-28 01:59:25 -0500

    ajuste

--- src/repository/abonoRepository.ts
+++ src/repository/abonoRepository.ts
@@ -1,7 +1,7 @@
 
 
 class AbonoRepository{ 
-  
+   
     public getIdAbono:string="select nextval('s_abono')";
     public getAbonosByDocumento:string="select * from abono where documento_id = $1";
   
