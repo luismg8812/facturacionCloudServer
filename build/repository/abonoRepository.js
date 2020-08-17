0a6564734d81416ca105fa8b958426a00cb0aff5
luis miguel gonzalez <luismg8812@hotmail.com>
2020-08-17 10:11:42 -0500

    ajuste

--- build/repository/abonoRepository.js
+++ build/repository/abonoRepository.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.abonoRepository = void 0;
 class AbonoRepository {
     constructor() {
         this.getIdAbono = "select nextval('s_abono')";
