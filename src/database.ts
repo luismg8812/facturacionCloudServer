1062ffcefc1a5914fc8328b144d9e26535c9e44e
luis miguel gonzalez <luismg8812@hotmail.com>
2020-08-24 10:03:02 -0500

    ajsute

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import pg from 'pg';
 import keys from './keys'
 
 
-const pool=new pg.Client(keys.database); //conexcion nube
+const pool=new pg.Client(keys.database); //conexcion nube 
 //const pool=new pg.Client(keys.database_local); //conexion local
 
 pool.connect().then(()=>console.log("connected susccessfuly"));
