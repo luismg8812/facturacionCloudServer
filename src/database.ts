98d37e28e80736e698350637582334b02432c664
luis miguel gonzalez <luismg8812@hotmail.com>
2020-11-21 11:33:22 -0500

    ajuste para conexion

--- src/database.ts
+++ src/database.ts
@@ -2,8 +2,8 @@ import pg from 'pg';
 import keys from './keys'
 
 
-//const pool=new pg.Client(keys.database_avenidagp); //conexcion nube 
-const pool=new pg.Client(keys.database_local); //conexion local
+const pool=new pg.Client(keys.database); //conexcion nube 
+//const pool=new pg.Client(keys.database_local); //conexion local
 
 pool.connect().then(()=>console.log("connected susccessfuly"));
 
