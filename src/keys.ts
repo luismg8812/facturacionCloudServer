4097e206b660f987afe838ba96b7ea3de47f5c45
luis miguel gonzalez <luismg8812@hotmail.com>
2021-01-08 14:53:38 -0500

    ajuste

--- src/keys.ts
+++ src/keys.ts
@@ -5,14 +5,15 @@ export default{
         password:'f4467b4ad687c77b212d8bbacd5583fcd937325cbee34d044ec7adfd50ee5d48',
         database:'d4mn73eg995brt',
         port:5432,
-        ssl: { rejectUnauthorized: false },
+        ssl: { rejectUnauthorized: false },  
+        
     },
     
     database_manjares:{
         host:'ec2-3-211-176-230.compute-1.amazonaws.com',
         user:'yoiygthxwcfhbe',
         password:'3b5298030c2a0e2035ceb79f764b21fa52f6f70cb7680f39b1976e706d778aa2',
-        database:'dddo4gdi3s15gq',
+        database:'dddo4gdi3s15gq',   
         port:5432,
         ssl: { rejectUnauthorized: false },
     },
