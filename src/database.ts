3eaa4fa5bb8cd76c158ced729ffa885d26fc76ab
effectivesoftware <luismg8812@gmail.com>
2021-07-20 15:36:45 -0500

    se debe compilar

--- src/database.ts
+++ src/database.ts
@@ -5,6 +5,6 @@ import keys from './keys'
 const pool=new pg.Client(keys.database_yacu); //conexcion nube 
 //const pool=new pg.Client(keys.database_local); //conexion local
 
-pool.connect().then(()=>console.log("connected susccessfuly yacu"));
+pool.connect().then(()=>console.log("connected susccessfuly yacu") );
 
 export default pool; 
